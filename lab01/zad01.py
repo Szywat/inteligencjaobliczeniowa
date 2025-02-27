598e7d3648caf342a7e5eaaac9a97e55b631eb5a
Szymon Wątorowski <szywat04@gmail.com>
2025-02-27 18:38:09 +0100

    +nie martw się, jutro będzie lepiej

--- lab01/zad01.py
+++ lab01/zad01.py
@@ -11,15 +11,30 @@ dzisiejsza_data = datetime.datetime.now()
 
 print(dzisiejsza_data)
 
-dni_do_narodzin = (dzisiejsza_data - urodziny).days
+dni_od_narodzin = (dzisiejsza_data - urodziny).days
 
-yP = math.sin((2*math.pi / 23)* dni_do_narodzin)
-yE = math.sin((2*math.pi / 28)* dni_do_narodzin)
-yI = math.sin((2*math.pi / 33)* dni_do_narodzin)
+yP = math.sin((2*math.pi / 23)* dni_od_narodzin)
+yE = math.sin((2*math.pi / 28)* dni_od_narodzin)
+yI = math.sin((2*math.pi / 33)* dni_od_narodzin)
+
+nastepny_dzien = dni_od_narodzin + 1
+
+nextYP = math.sin((2*math.pi / 23)* nastepny_dzien)
+nextYE = math.sin((2*math.pi / 28)* nastepny_dzien)
+nextYI = math.sin((2*math.pi / 33)* nastepny_dzien)
 
 print(f'Witaj {imie}')
-print(f'Twoje urodziny były tyle dni temu: {dni_do_narodzin} ')
+print(f'Twoje urodziny były tyle dni temu: {dni_od_narodzin} ')
 print('Twoje fale:')
 print(f' Fizyczna fala: {yP}')
 print(f' Emocjonalna fala: {yE}')
 print(f' Intelektualna fala: {yI}')
+
+if yP > 0.5 and yE > 0.5 and yI > 0.5:
+    print('Gratuluję dobrego wyniku!')
+elif yP < 0.5 or yE < 0.5 or yI < 0.5:
+    print('Nie przejmuj się :(')
+    if nextYP > yP or nextYE > yE or nextYI > yI:
+        print("Nie martw się. Jutro będzie lepiej!")
+
+# Spędzony czas: 1.5 - 2h
